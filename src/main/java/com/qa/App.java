50fcddc8e6f547127886b99200eab6aa4c360698
Warren <thewarrenbarton@gmail.com>
2024-03-05 13:29:48 +0000

    Added hello Claire

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Warren"));
+        System.out.println(sayHelloToSomeone("Claire"));
         System.out.println(sayGoodbye());
     }
 
